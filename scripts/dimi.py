212892f82e7156e7945320ef16babed7f6260af0
remo <1163925remo@gmail.com>
2025-07-03 13:49:02 +0200

    enables evaluating on pcfg itself

--- scripts/dimi.py
+++ scripts/dimi.py
@@ -512,9 +512,11 @@ class EarlyStopper:
                     logging.warning(f"Stopping training last best logodds were {self.best_tolerance} evals ago")
                     return False
         elif logodds > (self.last_probs - self.delta):
+            self.last_probs = logodds
             return True
         else:
             self.counter += 1
+            self.last_probs = logodds
             if self.counter > self.tolerance:
                 logging.warning(f"Stopping training last loggodds improvement was {self.tolerance} evals ago")
                 return False
