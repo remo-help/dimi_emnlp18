08e109f06360efb7058f3d06d5722ad22586aaae
remo <1163925remo@gmail.com>
2025-07-31 14:30:54 +0200

    debugging a memory leak

--- scripts/dimi.py
+++ scripts/dimi.py
@@ -57,9 +57,6 @@ def sample_beam(ev_seqs, params, working_dir, gold_seqs=None,
     K = int(params.get('k'))
     sent_lens = list(map(len, ev_seqs))
 
-    from pympler.tracker import SummaryTracker
-    tracker = SummaryTracker()
-
     max_len = max(map(len, ev_seqs))
 
     if params.get('max_len', 'None') != 'None':
@@ -263,8 +260,6 @@ def sample_beam(ev_seqs, params, working_dir, gold_seqs=None,
     last_model = False
     best_model = False
     ### Start doing actual sampling:
-    print('tracker before starting')
-    tracker.print_diff()
     while cur_iter < iters and continue_bool:
         sent_list = []
         pcfg_model.iter = cur_iter
@@ -419,15 +414,11 @@ def sample_beam(ev_seqs, params, working_dir, gold_seqs=None,
         #         pickle.dump(dnn_obs_model, rfn)
         #
         cur_iter += 1
-        print(f'tracker after iter {cur_iter}')
-        tracker.print_diff()
         if params.get("print_trees", False):
             p.join()
     if save_evals and save_logprobs:
         save_eval_probs(save_logprobs, working_dir, best_probs=False)
     logging.debug("Ending sampling")
-    print('tracker after sampling')
-    tracker.print_diff()
     workDistributer.stop()
 
     for cur_proc in range(0, num_cpu_workers + num_gpu_workers):
